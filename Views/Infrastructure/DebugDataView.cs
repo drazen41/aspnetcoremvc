324f7129051fc99ce5a1f9050e4c5214a51a23a5
drazen41 <drazen.svec@gmail.com>
2019-11-20 13:32:44 +0100

    Views

---
+++ Views/Infrastructure/DebugDataView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+
+namespace Views.Infrastructure
+{
+    public class DebugDataView : IView
+    {
+        public string Path => string.Empty;
+
+        public async Task RenderAsync(ViewContext context)
+        {
+            context.HttpContext.Response.ContentType = "text/plain";
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("--- Routing data ---");
+            foreach (var kvp in context.RouteData.Values)
+            {
+                sb.AppendLine($"Key:{kvp.Key}, Value:{kvp.Value}");
+            }
+            sb.AppendLine("---View Data ---");
+            foreach (var kvp in context.ViewData)
+            {
+                sb.AppendLine($"Key:{kvp.Key}, Value:{kvp.Value}");
+            }
+            await context.Writer.WriteAsync(sb.ToString());
+        }
+
+         
+    }
+}
---
+++ Views/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using Views.Infrastructure;
+
+namespace Views
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(); // dodaje razor
+            //services.Configure<MvcViewOptions>(options =>
+            //{
+            //    // options.ViewEngines.Clear(); // bez razora
+            //    options.ViewEngines.Insert(0, new DebugDataViewEngine());
+            //});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
