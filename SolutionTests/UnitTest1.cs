3038a7fdf8f92c3d0ba162bafc9dc278f062e9c4
drazen41 <drazen.svec@gmail.com>
2019-11-20 17:54:44 +0100

    ViewComponents

--- SolutionTests/UnitTest1.cs
+++ SolutionTests/UnitTest1.cs
@@ -1,6 +1,10 @@
 using System;
 using Xunit;
 using Moq;
+using UsingViewComponents.Models;
+using System.Collections.Generic;
+using UsingViewComponents.Components;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 
 namespace SolutionTests
 {
---
+++ UsingViewComponents/Components/CitySummary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Components
+{
+    public class CitySummary : ViewComponent 
+    {
+        private ICityRepository repository;
+        public CitySummary(ICityRepository repo)
+        {
+            repository = repo;
+            
+        }
+        public IViewComponentResult Invoke(bool showList)
+        {
+            if (showList)
+                return View("CityList", repository.Cities);
+            else
+            {
+                //string target = RouteData.Values["id"] as string;
+                //var cities = repository.Cities.Where(c => target == null || string.Compare(c.Country, target, true) == 0);
+                var cities = repository.Cities;
+                return View(new CityViewModel { Cities = cities.Count(), Population = cities.Sum(c => c.Population) });
+            }
+            //return $"{repository.Cities.Count()} cities," + $"{repository.Cities.Sum(c => c.Population)} people";
+            
+        }
+    }
+}
